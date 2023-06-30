80937bb12b13f69d8ead41cfb026b3b6f8bf9921
franckopheno <ngamofrancois527@gmail.com>
2023-06-30 19:21:33 +0100

    ajout du code des controlleur

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -2,9 +2,108 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Statut;
+use App\Models\Personnel;
+use App\Models\Status;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 
 class PersonnelController extends Controller
 {
-    //
+    //les methde de traitement
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = User::all();
+
+        return view('personnel.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(User $user):View
+    {
+        $status = Status::all();
+        return view('Personnel.create', compact('user', 'status'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        // dd($Personnel->id);
+        $cours=User::where('users_id','=',$user->id)->get();
+
+
+        return view('personnel.show',compact('cours'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+
+     $request->validate([
+       'statut_id' => 'required|exists:statuts,id'
+        ]);
+
+       $data = $request->except('_token');
+       $user->update($data);
+
+        return redirect()->route('personnel.index');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('Personnel.index');
+    }
+
+   /* public function guard()
+    {
+        $user = Auth::user();
+        if ($user->statut->titre_status != 'pharmacien') {
+            return $user;
+        } else {
+            return response()->json(['key' => 'l\'utilisateur n\'est pas un admin']);
+        }
+    }*/
 }
--- app/Http/Controllers/ReponseForumController.php
+++ app/Http/Controllers/ReponseForumController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MessageForum;
+use App\Models\ReponseForum;
 use Illuminate\Http\Request;
 
 class ReponseForumController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+    }
+
+    public function create(MessageForum $mesforum){
+
+        return view('forum.reponse', compact('mesforum'));
+    }
+
+    public function store(MessageForum $mesforum){
+        request()->validate([
+            'reponseforum' => 'required|min:2',
+
+        ]);
+
+        $reponse = new ReponseForum();
+        $reponse->reponseforum = request('reponseforum');
+        $reponse->user_id = auth()->user()->id;
+        $reponse->message_forums_id = $mesforum->id;
+
+        $reponse->save([$reponse]);
+        return redirect()->route('forum.index');
+    }
+
+     /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReponseForum  $repforum
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReponseForum $repforum)
+    {
+        $repforum->delete();
+
+    }
+
 }
