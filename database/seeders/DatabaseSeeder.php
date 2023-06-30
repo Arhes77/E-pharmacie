955dd8999927e124e65aefa4316ce29b618dfa9e
Arhes77 <133196936+Arhes77@users.noreply.github.com>
2023-06-30 20:09:04 +0100

    modif seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(StatutTableSeeder::class);
+        $this->call(StatusTableSeeder::class);
         $this->call(FamilleTableSeeder::class);
     }
 }
