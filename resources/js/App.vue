0ce6dcef59b581e90a1d4dca94730b4678cbaffb
WillySmith <113615116+Tenstorm7@users.noreply.github.com>
2023-07-02 10:55:53 +0100

    ajout du layout principale index + vue js

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="font-bold text-green-4500"> hello worlds</h1>
+    <input type="r" name="tele"  id="">
+</template>
+<script setup >
+
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,23 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from "vue/dist/vue.esm-bundler.js"
+import App from './App.vue';
+import Dropdown from './components/Dropdown.vue';
+import Logo from './components/Logo.vue';
+
 
 window.Alpine = Alpine;
+
+
+
+// 2. On importe AppComponent.vue
+const app = createApp();
+
+app.component('App',App);
+app.component('Logo',Logo);
+app.component('Dropdown',Dropdown);
+
+app.mount("#app")
 
 Alpine.start();
