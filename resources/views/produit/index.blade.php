ea1cdb35c9b626705a5f574a1316eb57de6cb187
WillySmith <113615116+Tenstorm7@users.noreply.github.com>
2023-07-04 16:39:02 +0100

    Ajout du footer

--- resources/views/produit/index.blade.php
+++ resources/views/produit/index.blade.php
@@ -1,56 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="{{route('produit.store')}}" method="post" enctype="multipart/form-data">
-    @csrf
-    <div>
+    <form action="{{ route('produit.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
         <div>
-            <label for="nom ">nom produit</label> 
-            <input type="text" name="nom_prod" id="nom" > 
-        </div>
+            <div>
+                <label for="nom ">nom produit</label>
+                <input type="text" name="nom_prod" id="nom">
+            </div>
 
-        <div>
-            <label for="descri">description</label> 
-            <input type="text" name="descri_prod" id="descri" > 
-        </div>
-        <div>
-            <label for="code">code produit</label> 
-            <input type="text" name="code_prod" id="code" > 
-        </div>
-        <div>
-            <label for="prix">prix</label> 
-            <input type="number" name="prix_prod" id="prix" > 
-        </div>
-        <div>
-            <label for="path">image du produit</label> 
-            <input type="file" name="url_prod" id="path" > 
-        </div>
-        <div>
-            <label for="qteS">qte en stock</label> 
-            <input type="number" name="qteS_prod" id="qteS" > 
-        </div>
+            <div>
+                <label for="descri">description</label>
+                <input type="text" name="descri_prod" id="descri">
+            </div>
+            <div>
+                <label for="code">code produit</label>
+                <input type="text" name="code_prod" id="code">
+            </div>
+            <div>
+                <label for="prix">prix</label>
+                <input type="number" name="prix_prod" id="prix">
+            </div>
+            <div>
+                <label for="path">image du produit</label>
+                <input type="file" name="url_prod" id="path">
+            </div>
+            <div>
+                <label for="qteS">qte en stock</label>
+                <input type="number" name="qteS_prod" id="qteS">
+            </div>
 
-        <div>
-            <label for="path">categorie du produit</label> 
-            <select name="categorie_id" id="path">
-                <option value="testcategorie">testcategorie</option>
-                <option value="test">test</option>
-                @foreach ($cats as $cat)
-                <option value="{{$cat->id}}" >{{$cat->nom_cat}}</option>
-                @endforeach
-                
-            </select>
-            
-        </div>
-        <input type="submit" value="creer le produit">
-    </div>
+            <div>
+                <label for="path">categorie du produit</label>
+                <select name="categorie_id" id="path">
+                    @foreach ($cats as $cat)
+                        <option value="{{ $cat->id }}">{{ $cat->nom_cat }}</option>
+                    @endforeach
 
-       
+                </select>
+
+            </div>
+            <input type="submit" value="creer le produit">
+        </div>
+    </form>
+
+    {{-- <br>
+    <form action="{{ route('categorie.store',[3]) }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div>
+            <div>
+                <label for="nom_cat">nom categorie</label>
+                <input type="text" name="nom_cat" id="nom">
+            </div>
+
+            <div>
+                <label for="descri_cat">description categorie</label>
+                <input type="text" name="descri_cat" id="descri">
+            </div>
+            <div>
+            <input type="submit" value="creer le produit">
+        </div>
+    </form> --}}
+
 </body>
+
 </html>
