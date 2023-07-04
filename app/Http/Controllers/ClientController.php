c683c3b3a21e08f6c4bbd704259846b5f62714bd
Arhes77 <133196936+Arhes77@users.noreply.github.com>
2023-07-04 16:15:14 +0100

    mise a jour

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Status;
 use App\Models\Personnel;
-use Illuminate\Database\Eloquent\Model;
-use Symfony\Component\HttpFoundation\Request;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
