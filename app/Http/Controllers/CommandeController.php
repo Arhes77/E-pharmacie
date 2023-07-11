77bf6a1310934cf0ca159175d1c2c9bf29f8be90
elcapo21 <arnaudfoubeuda@gmail.com>
2023-07-11 15:11:01 +0100

    ajout du paiement par stripe

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 class CommandeController extends Controller
 {
     public function index( Request $request){
-        return view('commande.index',compact('request'));
+        $panier=$request->panier;
+        $prix=$request->prix;
+        return view('commande.index')->with(
+            [
+                'panier'=>$panier,
+                'prix'=>$prix,
+            ]
+        );
+    }
+
+    public function show(Request $req){
+        
+        $prix=$req->prix;
+        $panier=$req->panier;
+        return view('stripe')->with(
+            [
+                
+                'prix'=>$prix,
+                'panier'=>$panier,
+            ]
+        ); 
     }
 }
